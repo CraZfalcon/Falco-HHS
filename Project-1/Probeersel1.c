9d1e2ae0f8884062dfc9c8a31eb0af0510b367bb
Falco Storm <falcostorm10@gmail.com>
2023-09-04 11:15:34 +0200

    04-09-2023

--- Project-1/Probeersel1.c
+++ Project-1/Probeersel1.c
@@ -1,7 +1,41 @@
+#pragma warning(disable : 4996)
 #include <stdio.h>
 
-int main(void) {
-    
-	printf("Hello, World!");
+ int main(void) {
+	int a, b;
+	printf("Geef het eerste getal: ");
+	scanf("%d", &a);
+	printf("Geef een tweede getal: ");
+	scanf("%d", &b);
+	
+
+	printf("Het product van %d en %d is: %d\n", a, b, a * b);
+	printf("\nSluit dit venster door op enter te drukken");
+	getchar();
 	return 0;
 }
+
+ /*#pragma warning(disable : 4996)
+#include <stdio.h>
+#include <math.h>
+
+ // For Visual Studio
+#pragma warning(disable : 4996)
+
+ int main(void) {
+	 double x, y, wortel, macht;
+	 int i;
+	 printf("Voer een positief getal x in: ");
+	 scanf("%lf", &x);
+	 printf("Voer een getal y in: ");
+	 scanf("%lf", &y);
+	 for (i = 0; i < 5; i = i + 1) {
+		 wortel = sqrt(x);
+		 macht = pow(x, y);
+		 printf("De vierkantswortel uit %f is: %f\n", x, wortel);
+		 printf("%f tot de macht %f is: %f\n", x, y, macht);
+		 x = x + 1;
+	 }
+	 getchar();
+	 return 0;
+ }*/
