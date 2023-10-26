14d5b95aee8a1af31ead53da91beac10f309fca0
Falco Storm <falcostorm10@gmail.com>
2023-10-26 15:03:19 +0200

    26-10-2023

---
+++ CPROUC-Test1/Reverse.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+char raw[] = "Hello World!";
+
+int strlen(char* str) {
+	int len = 0;
+	while (str[len] != '\0') {
+		len++;
+	}
+	return len;
+}
+
+
+int main(void) {
+
+	for (int i = 0; i < strlen(raw); i++) printf("%c", raw[strlen(raw) - i - 1]);
+
+	/*
+	
+		int len = strlen(raw);
+
+		for (int i = 0; i < len; i++) {
+			printf("%c", raw[len - i - 1]);
+		}
+	
+	*/
+
+}
